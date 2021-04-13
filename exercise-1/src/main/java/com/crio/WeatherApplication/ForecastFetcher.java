a84748d259ad9e510f30db3fd47ecb508472fb9a
sridherj <sridherj@criodo.com>
2021-04-13 14:13:49 +0530

    Updates to build.gradle

--- exercise-1/src/main/java/com/crio/WeatherApplication/ForecastFetcher.java
+++ exercise-1/src/main/java/com/crio/WeatherApplication/ForecastFetcher.java
@@ -2,7 +2,6 @@ package com.crio.WeatherApplication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
